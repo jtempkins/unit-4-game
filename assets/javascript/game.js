68587f0712d0bb1bbd6f17ec9be04b6605d6dc3d
Joy Tempkins <jtempkins@gmail.com>
2019-08-13 18:56:41 -0500

    added random number display to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
     // allowing html to load before beginning any functions
     $(document).ready(function() {
-
+    // Selects random number between 19 and 120
+    var randomNumber = Math.floor(Math.random() * 101 + 19)
+        
+    $("#numberToGuess").text(randomNumber);
+    console.log (randomNumber);
 
 
 
