73fd45e3fbbcfb8b38509fb2dcbb54e72e0bb706
Joy Tempkins <jtempkins@gmail.com>
2019-08-14 22:52:52 -0500

    added crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,16 @@
-    // allowing html to load before beginning any functions
+var gamescore = 0;
+var crystalcollected = 0;
+var wins = 0;
+var loss =0;
+var startGame=("");
+var userTotal = 0;
+var num1= Math.floor(Math.random()*11+1)
+var num2= Math.floor(Math.random()*11+1)
+var num3= Math.floor(Math.random()*11+1)
+var num4= Math.floor(Math.random()*11+1)
+
+
+// allowing html to load before beginning any functions
     $(document).ready(function() {
     // Selects random number between 19 and 120
     var randomNumber = Math.floor(Math.random() * 101 + 19)
@@ -6,9 +18,37 @@
     $("#numberToGuess").text(randomNumber);
     console.log (randomNumber);
 
+ //Global 
+        
 
+   //Initial Start game
+ function startGame(){
+   gamescore = Math.random()
+   var  rnd1 = Math.random()*12
+   // 
+   $("#img1").attr("data-value",rnd1) //Assign random value
+   $("#img2").attr("data-value",rnd1) //Assign random value
+   $("#img3").attr("data-value",rnd1) //Assign random value
+   $("#img4").attr("data-value",rnd1) //Assign random value
 
-
-
-
-});
+   crystalcollected = 0;
+   
+ }
+  })  
+///// On Click
+   $('#Img1').on("click",function(){
+        var crystal = $(this).attr("data-value"); // On Click - collect the value
+        crystalcollected += crystal;
+        if(crystalcollected> gamescore){
+            loss++;
+            $('#loss').text();
+            //....
+            startGame();
+        }
+        else if(gamescore === crystalcollected){
+            wins++
+            $('#wins').text();
+            // .... $().text()
+            startGame();
+        }
+   })
