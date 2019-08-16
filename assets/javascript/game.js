a5fde169f640da452214700f77d2ad54700f3374
Joy Tempkins <jtempkins@gmail.com>
2019-08-15 23:28:38 -0500

    added click totals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,20 @@
-var gamescore = 0;
-var crystalcollected = 0;
-var wins = 0;
-var loss =0;
-var startGame=("");
-var userTotal = 0;
-var num1= Math.floor(Math.random()*11+1)
-var num2= Math.floor(Math.random()*11+1)
-var num3= Math.floor(Math.random()*11+1)
-var num4= Math.floor(Math.random()*11+1)
+var gamescore
+var crystalcollected
+var wins
+var losses
+var startGame
+var userTotal
+var num1
+var num2
+var num3
+var num4
 
 
 // allowing html to load before beginning any functions
-    $(document).ready(function() {
+$(document).ready(function() {
+    startGame()
     // Selects random number between 19 and 120
-    var randomNumber = Math.floor(Math.random() * 101 + 19)
+    var randomNumber = Math.floor(Math.random() * 101 + 19);
         
     $("#numberToGuess").text(randomNumber);
     console.log (randomNumber);
@@ -23,32 +24,93 @@ var num4= Math.floor(Math.random()*11+1)
 
    //Initial Start game
  function startGame(){
-   gamescore = Math.random()
-   var  rnd1 = Math.random()*12
-   // 
-   $("#img1").attr("data-value",rnd1) //Assign random value
-   $("#img2").attr("data-value",rnd1) //Assign random value
-   $("#img3").attr("data-value",rnd1) //Assign random value
-   $("#img4").attr("data-value",rnd1) //Assign random value
-
-   crystalcollected = 0;
+  gamescore = 0;
+  crystalcollected = 0;
+  wins = 0;
+  losses = 0;
+  startGame=("");
+  userTotal = 0;
+  num1= Math.floor(Math.random()*11+1)
+  num2= Math.floor(Math.random()*11+1)
+  num3= Math.floor(Math.random()*11+1)
+  num4= Math.floor(Math.random()*11+1)
    
  }
-  })  
-///// On Click
-   $('#Img1').on("click",function(){
-        var crystal = $(this).attr("data-value"); // On Click - collect the value
-        crystalcollected += crystal;
-        if(crystalcollected> gamescore){
-            loss++;
-            $('#loss').text();
-            //....
-            startGame();
+
+ // Set win score and restart game ... startgame ()
+function addWin() {
+  wins++
+  startGame()
+}
+//sets up click for jewels
+$('#img1').on ('click', function(){
+    gamescore = gamescore + num1;
+    console.log("New userTotal= " + crystalcollected);
+    $('#gamescore').text(gamescore); 
+          //sets win/lose conditions
+          if ( gamescore > randomNumber){
+            // addloses();
+            // $('#losses').text();
+            losses++;
+          }   
+          else if (gamescore === randomNumber){
+          // addWin();
+          // $('#win').text();
+          wins++;
+            // $('#wins').text();
+
         }
-        else if(gamescore === crystalcollected){
-            wins++
-            $('#wins').text();
-            // .... $().text()
-            startGame();
-        }
-   })
+  })  ;
+
+  $('#img2').on ('click', function(){
+    gamescore = gamescore + num2;
+    console.log("New userTotal= " + crystalcollected);
+    $('#gamescore').text(gamescore); 
+          //sets win/lose conditions
+          if ( gamescore > randomNumber){
+            // addloses();
+            // $('#losses').text();
+            losses++
+          }   
+          else if (gamescore === randomNumber){
+          // addWin();
+          // $('#win').text();
+          wins++;
+       }
+      });
+  
+  $('#img3').on ('click', function(){
+     gamescore = gamescore + num3;
+     console.log("New userTotal= " + crystalcollected);
+     $('#gamescore').text(gamescore); 
+          //sets win/lose conditions
+        if ( gamescore > randomNumber){
+          //  addlosses();
+          // $('#losses').text();
+           losses++
+              }
+        else if (gamescore === randomNumber){
+                // addWin();
+                // $('#win').text();
+                wins++
+                     }
+                   });   
+
+    $('#img4').on('click', function(){
+      gamescore = gamescore + num4;
+      console.log("New userTotal= " + crystalcollected);
+        $('#gamescore').text(gamescore); 
+        //sets win/lose conditions
+    
+          if (gamescore > randomNumber){
+              // addlosses();
+              // $('#losses').text();
+              losses++
+       }
+         else if ( gamescore === randomNumber){
+          // $('#win').text();
+          win++;
+    }
+  })
+  $("#gamescore").text(userTotal);
+})
