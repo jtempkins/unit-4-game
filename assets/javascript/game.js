8d79de52823d828a5111b42a090c74e16b558232
Joy Tempkins <jtempkins@gmail.com>
2019-08-17 07:53:19 -0500

    tidied up js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,7 @@ var continueGame
 // allowing html to load before beginning any functions
 $(document).ready(function() {
     startGame()
+
     // Selects random number between 19 and 120
     var randomNumber = Math.floor(Math.random() * 101 + 19);
         
@@ -25,12 +26,11 @@ $(document).ready(function() {
 
    //Initial Start game
  function startGame(){
-  gamescore = 0;		//uncommented this line as this is where you were resetting your score from the buttons
+  gamescore = 0;
   crystalcollected = 0;
   wins = 0;
   losses = 0;
   startGame=("");
-  //userTotal = 0;		//commented this out as it is never used in the entire page
   num1= Math.floor(Math.random()*11+1)
   num2= Math.floor(Math.random()*11+1)
   num3= Math.floor(Math.random()*11+1)
@@ -39,41 +39,25 @@ $(document).ready(function() {
  }
 
  function continueGame(){
-  gamescore = 0;		//uncommented this as well same as the other line
+  gamescore = 0;
   randomNumber = Math.floor(Math.random() * 101 + 19);
   crystalcollected = 0;
-  // wins = 0;
-  // losses = 0;
   startGame=("");
-  //userTotal = 0;		//still wasnt used
   num1= Math.floor(Math.random()*11+1)
   num2= Math.floor(Math.random()*11+1)
   num3= Math.floor(Math.random()*11+1)
   num4= Math.floor(Math.random()*11+1)
-  $("#numberToGuess").text(randomNumber);	// added so that it will actually show new number when you do this function
+  $("#numberToGuess").text(randomNumber);
    
  }
-
- 
-//  // Set win score and restart game ... startgame ()
-// function addWin() {
-//   wins++
-//   continueGame()
-// }
-
-// function addlosses() {
-//   losses++
-//   continueGame()
-// }
-
-
 
 //sets up click for jewels
 $('#img1').on ('click', function(){
     gamescore = gamescore + num1;
     console.log("New userTotal= " + crystalcollected);
     $('#gamescore').text(gamescore); 
-          //sets win/lose conditions
+
+    //sets win/lose conditions
           if ( gamescore > randomNumber){
             losses++;
             $('#losses').text(losses);
@@ -86,14 +70,15 @@ $('#img1').on ('click', function(){
           continueGame();
 
         }
-		$("#gamescore").text(gamescore);	//added this so that after you win it will show that your new score is at 0
+		$("#gamescore").text(gamescore);
   })  ;
 
   $('#img2').on ('click', function(){
     gamescore = gamescore + num2;
     console.log("New userTotal= " + crystalcollected);
     $('#gamescore').text(gamescore); 
-          //sets win/lose conditions
+
+    //sets win/lose conditions
           if ( gamescore > randomNumber){
             losses++;
             $('#losses').text(losses);
@@ -105,13 +90,14 @@ $('#img1').on ('click', function(){
           $('#wins').text(wins);
 		  continueGame();
        }
-	   $("#gamescore").text(gamescore);		//added this so that after you win it will show that your new score is at 0
+	   $("#gamescore").text(gamescore);
       });
   
   $('#img3').on ('click', function(){
      gamescore = gamescore + num3;
      console.log("New userTotal= " + crystalcollected);
      $('#gamescore').text(gamescore); 
+
      //sets win/lose conditions
      if ( gamescore > randomNumber){
        losses++;
@@ -124,13 +110,14 @@ $('#img1').on ('click', function(){
      $('#wins').text(wins);
 	 continueGame();
                 }
-				$("#gamescore").text(gamescore);		//added this so that after you win it will show that your new score is at 0
+				$("#gamescore").text(gamescore);
                    });   
 
     $('#img4').on('click', function(){
       gamescore = gamescore + num4;
       console.log("New userTotal= " + crystalcollected);
       $('#gamescore').text(gamescore); 
+  
       //sets win/lose conditions
       if ( gamescore > randomNumber){
         losses++;
@@ -143,7 +130,7 @@ $('#img1').on ('click', function(){
       $('#wins').text(wins);
 	  continueGame();
 }
-$("#gamescore").text(gamescore);		//added this so that after you win it will show that your new score is at 0
+$("#gamescore").text(gamescore);
   })
   $("#gamescore").text(gamescore);		
 })
